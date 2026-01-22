8fb08bfa5ac4e33c2c54d49b9688ea9a7068ca30
NEKROO <xjwyes@gmail.com>
2026-01-22 22:23:07 +0800

    delete files

--- v1/logic/__init__.py
+++
@@ -1,6 +0,0 @@
-# Logic module initialization
-from .git_guard import GitGuard
-from .dispatcher import Dispatcher
-from .planner import Planner
-from .implementor import Implementor
-from .verifier import Verifier
--- v1/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Test Suite for L4D V2 AST-Based Context Collection
-
-This package contains unit and integration tests for the new AST analysis components.
-"""
